ca984c71837d8d168692102e741c08135daeeb15
Knight Huang <knighthuang@91app.com>
2020-04-28 09:14:29 +0800

    200428 - Slack Tag Team

--- src/Phanerozoic.Core.Test/Services/SlackServiceTests.cs
+++ src/Phanerozoic.Core.Test/Services/SlackServiceTests.cs
@@ -17,11 +17,43 @@ namespace Phanerozoic.Core.Services.Tests
         }
 
         [Fact(Skip = "整合測試")]
-        public void SendAsyncTest()
+        public void Test_Tag_User()
         {
             //// Arrange
             string webHookUrl = "";
             var data = new { text = "<@UserID|cal> bla bla" };
+            var json = JsonSerializer.Serialize(data);
+
+            //// Act
+            var target = this.GetTarget();
+            target.SendAsync(webHookUrl, json);
+
+            //// Assert
+            Assert.True(true);
+        }
+
+        [Fact(Skip = "整合測試")]
+        public void Test_Tag_Group_With_Link_Names()
+        {
+            //// Arrange
+            string webHookUrl = "https://hooks.slack.com/services/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";
+            var data = new { text = "@oversea_brd 我是測試訊息 with link names", link_names = 1 };
+            var json = JsonSerializer.Serialize(data);
+
+            //// Act
+            var target = this.GetTarget();
+            target.SendAsync(webHookUrl, json);
+
+            //// Assert
+            Assert.True(true);
+        }
+
+        [Fact(Skip = "整合測試")]
+        public void Test_Tag_Group_With_Subteam_Id()
+        {
+            //// Arrange
+            string webHookUrl = "";
+            var data = new { text = "<!subteam^SJGSP4PFW> 我是測試訊息 with subteam id" };
             var json = JsonSerializer.Serialize(data);
 
             //// Act
