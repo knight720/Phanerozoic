0f1b7cd6193f10e31cc8992c8c162f0410230b56
Knight Huang <knighthuang@91app.com>
2020-10-16 09:22:52 +0800

    201016 - 忽略 Column 不符合長度的的 Row

---
+++ src/Phanerozoic.Core/Helpers/EnumerableHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Phanerozoic.Core.Helpers
+{
+    public static class EnumerableHelper
+    {
+        /// <summary>
+        /// List To String
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumerable">The enumerable.</param>
+        /// <returns></returns>
+        public static string EnumerableToString<T>(this IEnumerable<T> enumerable)
+        {
+            var stringBuilder = new StringBuilder();
+            var stringArray = enumerable.Select(i => i.ToString()).ToArray();
+            return string.Join(", ", stringArray);
+        }
+    }
+}
