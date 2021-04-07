a447a532ba7783c477fef324b8bcb121bd2dab2b
Knight Huang <knighthuang@91app.com>
2021-04-07 09:43:02 +0800

    210407 - Create GovementHolidayService

--- src/Phanerozoic.Core/Services/Holidays/GovementHolidayService.cs
+++ src/Phanerozoic.Core/Services/Holidays/GovementHolidayService.cs
@@ -1,21 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Phanerozoic.Core.Services.Interfaces;
 
 namespace Phanerozoic.Core.Services.Holidays
 {
     public class GovementHolidayService : IHolidayService
     {
-        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly HttpClient _httpClient;
 
         public GovementHolidayService(IHttpClientFactory httpClientFactory)
         {
-            this._httpClientFactory = httpClientFactory;
+            this._httpClient = httpClientFactory.CreateClient();
         }
 
         public bool IsHoliday(DateTime date)
         {
-            throw new NotImplementedException();
+            var url = "https://data.ntpc.gov.tw/api/datasets/308DCD75-6434-45BC-A95F-584DA4FED251/json?page=1&size=950";
+            var httpResponseMessage = this._httpClient.GetAsync(url).GetAwaiter().GetResult();
+            Console.WriteLine($"StatusCode: {httpResponseMessage.StatusCode}");
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                var content = httpResponseMessage.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                var holidayList = JsonSerializer.Deserialize<IList<Holiday>>(content);
+                var holiday = holidayList.FirstOrDefault(i => i.Date.Equals(date.Date));
+                if (holiday == null)
+                {
+                    return false;
+                }
+
+                Console.WriteLine(holiday.ToString());
+                return true;
+            }
+            return false;
+        }
+
+        private class Holiday
+        {
+            [JsonPropertyName("date")]
+            public string Date { get; set; }
+
+            [JsonPropertyName("name")]
+            public string Name { get; set; }
+
+            [JsonPropertyName("isHoliday")]
+            public string IsHoliday { get; set; }
+
+            [JsonPropertyName("holidayCategory")]
+            public string HolidayCategory { get; set; }
+
+            [JsonPropertyName("description")]
+            public string Description { get; set; }
+
+            public override string ToString()
+            {
+                var result = "{Date}, {Name}, {IsHoliday}, {HolidatyCategory}, {Description}";
+                return result;
+            }
         }
     }
 }
